ee8cf671c74658bc38dc44a4d3e83280b8a6e4ab
gristar <sheng.fighting@qq.com>
2017-07-19 18:12:56 +0800

    路由用列表成列出来，增加用户查询增加功能，修改layout

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
  * Created by gristar on 2017/7/12.
  */
 const Schema = require('mongoose').Schema;
-const User = Schema({name: String});
+const User = Schema({username: String, password: String});
 const db = require('./db');
 
 module.exports = db.model('User', User);
--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const user = require('../models/user');
+const User = require('../models/user');
 
-/* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource');
+    User.find(function(err, docs){
+        if(err) return next(err);
+        res.render('users', {list:docs});
+    });
 });
 
 router.get('/query/:id', function (req, res, next) {
     const id = req.params.id;
     console.log(id, 'user id');
-    user.find(function(err, docs){
+    User.findOne({id:id},function(err, docs){
         if(err) return next(err);
         res.send(docs);
     })
 });
 
 router.get('/insert', function (req, res, next){
-    res.render('users');
+    res.render('user-add');
 });
 
 router.get('/insert/add', function (req, res, next){
-    console.log(req.param);
+    new User(req.body).save(function(err, user){
+        if(err) return next(err);
+        res.location('/users');
+    });
 });
 
 module.exports = router;
